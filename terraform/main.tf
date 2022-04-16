15c02bd82f28bccbcf2b59fc79b22f2d0531d91c
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2022-04-16 16:41:36 -0700

    Test autopilot cluster + issues workflow

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+resource "google_container_cluster" "primary" {
+  name               = "chatbot-dev-1"
+  location           = "us-west1"
+  initial_node_count = 1
+
+  # Enabled for GCP free tier
+  enable_autopilot = true
+
+  # TODO: Make nodes private
+  # master_authorized_networks_config {
+  #   cidr_blocks {
+  #     cidr_block = "REDACTED"
+  #     display_name = "Home"
+  #   }
+  # }
+
+  # private_cluster_config {
+  #   enable_private_nodes = true
+  #   enable_private_endpoint = false
+  #   master_ipv4_cidr_block = "172.16.0.0/28"
+  # }
+}
